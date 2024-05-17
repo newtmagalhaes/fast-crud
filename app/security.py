024d24bf21fa24d9d0f3fbdf16503ff3c2c2911b
anilton <aniltonmagalhaes@gmail.com>
2024-05-17 00:24:49 -0300

    wip: add login and hash

--- app/security.py
+++ app/security.py
@@ -1,6 +1,32 @@
+from datetime import datetime, timedelta
+from os import environ
+from zoneinfo import ZoneInfo
+
+from jwt import encode
+from passlib.context import CryptContext
 from pydantic import SecretStr
 
+_SECRET_KEY = environ["SECRET_KEY"]
+_ALGORITHM = 'HS256'
+_ACCESS_TOKEN_EXPIRE_MINUTES = 30
+_pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-def hash_password(password: SecretStr) -> str:
-    # TODO: resolve hashing
-    return password.get_secret_value()
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = (
+        datetime.now(tz=ZoneInfo('UTC'))
+        + timedelta(minutes=_ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    to_encode.update({'exp': expire})
+
+    return encode(to_encode, _SECRET_KEY, algorithm=_ALGORITHM)
+
+
+def hash_password(password: str) -> str:
+    return _pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return _pwd_context.verify(plain_password, hashed_password)
